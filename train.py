51e65d8c401a0003398c8a20f647603bb0ddcfc7
ErtugrulSen <senertugrul91@gmail.com>
2023-08-07 10:12:36 +0300

    updated train model

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, Trainer, TrainingArguments, DataCollatorWithPadding
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, Trainer, TrainingArguments
 from datasets import Dataset
 
 data = pd.read_csv("data/sikayetimVar.csv")
@@ -17,28 +17,27 @@ label2id = {label: i for i, label in enumerate(train_data["category"].unique())}
 train_labels = [label2id[label] for label in train_data["category"]]
 test_labels = [label2id[label] for label in test_data["category"]]
 
-train_dataset = Dataset.from_dict({"input_ids": train_encodings["input_ids"], "attention_mask": train_encodings["attention_mask"], "labels": train_labels})
-test_dataset = Dataset.from_dict({"input_ids": test_encodings["input_ids"], "attention_mask": test_encodings["attention_mask"], "labels": test_labels})
+train_dataset = Dataset.from_dict({"input_ids": train_encodings["input_ids"], "attention_mask": train_encodings["attention_mask"], "label": train_labels})
+test_dataset = Dataset.from_dict({"input_ids": test_encodings["input_ids"], "attention_mask": test_encodings["attention_mask"], "label": test_labels})
 
 model = DistilBertForSequenceClassification.from_pretrained(model_name, num_labels=len(label2id))
 
 training_args = TrainingArguments(
     output_dir="./models",
-    num_train_epochs=3,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    warmup_steps=500,
-    weight_decay=0.01,
+    num_train_epochs=10,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    logging_steps=500,
+    save_steps=1000,
+    logging_dir="./logs",
 )
-
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=test_dataset,
-    data_collator=data_collator,
 )
 trainer.train()
 model.save_pretrained("models")
+
